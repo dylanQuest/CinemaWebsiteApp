9751b0983e00ca4642386bc6806bd1bb160577ef
L00169531 <L00169531@atu.ie>
2024-03-15 11:28:23 +0000

    Finished Home Page and added details for each clicked film which shows a list of showtimes and a trailer

--- tut1 - Copy - Copy/Pages/Customer/Home/Details.cshtml.cs
+++ tut1 - Copy - Copy/Pages/Customer/Home/Details.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RP1.Models.Models;
+using RP1.Services;
 
 namespace tut1.Pages.Customer.Home
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DetailsModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Film Film { get; set; }
+        public IEnumerable<Screening> listOfScreenings { get; set; }
+
+        public void OnGet(int id)
+        {
+            Film = _unitOfWork.FilmRepo.Get(id);
+
+            listOfScreenings = _unitOfWork.ScreeningRepo.GetAll();
+
+            listOfScreenings = listOfScreenings.Where(p => p.filmId == Film.Id);
         }
     }
 }
--- tut1 - Copy - Copy/Pages/Customer/Home/Order.cshtml.cs
+++ tut1 - Copy - Copy/Pages/Customer/Home/Order.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RP1.Models.Models;
+using RP1.Services;
+using System.Drawing.Drawing2D;
 
 namespace tut1.Pages.Customer.Home
 {
     public class OrderModel : PageModel
     {
-        public void OnGet()
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderModel(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IEnumerable<Screening> listOfScreenings { get; set; }
+		public Film Film { get; set; }
+
+		public IEnumerable<Film> listOfFilms { get; set; }
+		public void OnGet(int Id)
         {
-        }
+			Film = _unitOfWork.FilmRepo.Get(Id);
+			listOfFilms = _unitOfWork.FilmRepo.GetAll();
+			listOfScreenings = _unitOfWork.ScreeningRepo.GetAll();
+
+			listOfScreenings = listOfScreenings.Where(p => p.filmId == Film.Id);
+		}
     }
 }
