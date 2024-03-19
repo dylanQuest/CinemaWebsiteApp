c97889efe7a361784c995eacba88c6d81ca6cbe1
L00169531 <L00169531@atu.ie>
2024-03-19 01:48:58 +0000

    booking page added, points to the chosen screening and gets a list of ticket types. need to: calc total cost depending on how many seats booked(maybe just put this in followup screen) and add an onpost which creates a booking and ticket object in database

--- tut1 - Copy - Copy/Pages/Customer/Home/Book.cshtml.cs
+++ tut1 - Copy - Copy/Pages/Customer/Home/Book.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RP1.Models.Models;
+using RP1.Services;
+using Type = RP1.Models.Models.Type;
 
 namespace tut1.Pages.Customer.Home
 {
     public class BookModel : PageModel
     {
-        public void OnGet()
+		private readonly IUnitOfWork _unitOfWork;
+
+
+		public Screening Screening { get; set; }
+
+		public Booking Booking { get; set; }
+
+		public Type Type { get; set; }
+
+		public IEnumerable<Type> listOfTypes { get; set; }
+
+		public Ticket Ticket { get; set; }
+
+		public BookModel(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public void OnGet()
         {
+			listOfTypes = _unitOfWork.TypeRepo.GetAll();
         }
-    }
+
+	}
 }
