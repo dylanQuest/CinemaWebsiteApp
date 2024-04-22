39765066be2743bc3b0d950fce10b420d1720a6f
dylanQuest <l00169531@atu.ie>
2024-04-22 12:11:13 +0100

    made trailer a popup and added skeleton for emailing users

--- tut1 - Copy - Copy/Pages/Customer/Home/BookingMade.cshtml.cs
+++ tut1 - Copy - Copy/Pages/Customer/Home/BookingMade.cshtml.cs
@@ -9,35 +9,7 @@ namespace tut1.Pages.Customer.Home
 {
     public class BookingMadeModel : PageModel
     {
-		private readonly IUnitOfWork _unitOfWork;
 
-
-		public Screening Screening { get; set; }
-
-		public Booking Booking { get; set; }
-
-		public Type Type { get; set; }
-
-		public IEnumerable<SelectListItem> listOfTypesItems { get; set; }
-		public IEnumerable<Type> listOfTypes { get; set; }
-
-		public Ticket Ticket { get; set; }
-
-		public BookingMadeModel(IUnitOfWork unitOfWork)
-		{
-			_unitOfWork = unitOfWork;
-		}
-		public void OnGet()
-        {
-			//listOfTypes = _unitOfWork.TypeRepo.GetAll();
-			listOfTypesItems = _unitOfWork.TypeRepo.GetAll().Select(i => new SelectListItem()
-			{
-				Text = i.TypeName,
-				Value = i.Id.ToString(),
-			});
-
-			listOfTypes = _unitOfWork.TypeRepo.GetAll();
-		}
 
 	}
 }
