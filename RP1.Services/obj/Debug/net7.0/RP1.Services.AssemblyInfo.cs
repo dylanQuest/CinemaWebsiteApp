fcada51d0a98de2f11d2e321d393b0e90e076b92
L00169531 <L00169531@atu.ie>
2024-04-25 12:08:15 +0100

    made booking page look a bit better

--- RP1.Services/obj/Debug/net7.0/RP1.Services.AssemblyInfo.cs
+++ RP1.Services/obj/Debug/net7.0/RP1.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RP1.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0861c966675fa079f272789e4cdea2f03a9a1542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RP1.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RP1.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
