7c432bd6b747c506a6b75317e1c01d833289cca2
RadicalCheese <L00164316@atu.ie>
2024-05-01 19:51:13 +0100

    changes

--- tut1 - Copy - Copy/obj/Debug/net7.0/tut1.AssemblyInfo.cs
+++ tut1 - Copy - Copy/obj/Debug/net7.0/tut1.AssemblyInfo.cs
@@ -14,7 +14,15 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tut1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c6ed6c19309c3ee21a8c677f18ce16f9022a09")]
+=======
+<<<<<<< HEAD
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c6ed6c19309c3ee21a8c677f18ce16f9022a09")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+>>>>>>> 40e208c31359e390fad1c8fd623b18030299c51b
+>>>>>>> parent of 8c3538b (changes)
 [assembly: System.Reflection.AssemblyProductAttribute("tut1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tut1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
